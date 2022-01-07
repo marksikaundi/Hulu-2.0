8e6d835ee178632be114d58f99afb0c81c089057
marksikaundi <emmanuelmark117@gmail.com>
2022-01-07 16:21:46 +0200

    all error

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,20 @@
+import { BadgeCheck, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon, } from "heroicons-react"
+import Image from "next/image"
+import HeaderItem from "./HeaderItem"
+
 function Header() {
     return (
         <header className="">
-            <h1>this is the Header</h1>
+            <div>
+                <HeaderItem title='Home' Icon={HomeIcon} />
+            </div>
+
+            <Image 
+            className="object-contain"
+            src="https://links.papareact.com/ua6"
+            width={200}
+            height={100}
+            />
         </header>
     )
 }
