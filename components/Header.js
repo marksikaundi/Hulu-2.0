0d39e6cb399173c0fcb021d30eaa2d2dabb0eb5b
marksikaundi <emmanuelmark117@gmail.com>
2022-01-08 13:09:09 +0200

    done with header

--- components/Header.js
+++ components/Header.js
@@ -13,8 +13,8 @@ import HeaderItem from "./HeaderItem"
 
 function Header() {
     return (
-        <header className="">
-            <div>
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
                 <HeaderItem title='HOME' Icon={HomeIcon} /> 
                 <HeaderItem title='TRENDING' Icon={LightningBoltIcon} /> 
                 <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} /> 
