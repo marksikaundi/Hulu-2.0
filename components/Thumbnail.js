262ac4c7208ed63999fddcb93b575d932cb28611
marksikaundi <emmanuelmark117@gmail.com>
2022-01-08 23:23:22 +0200

    hulu clone build done

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,11 @@
 import Image from "next/image"
 import { ThumbUpIcon } from "@heroicons/react/outline"
+import { forwardRef } from "react"
 
-function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL ="https://image.tmdb.org/t/p/original/"
     return (
-        <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+        <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image 
             layout="responsive"
             src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`
@@ -21,6 +22,6 @@ function Thumbnail({ result }) {
             </div>
         </div>
     )
-}
+})
 
 export default Thumbnail
