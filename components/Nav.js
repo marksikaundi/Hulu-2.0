cd17f6f0ec8c4b3e009e95930221db5e143012f6
marksikaundi <emmanuelmark117@gmail.com>
2022-01-08 14:26:49 +0200

    Nav and api config

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,13 @@
+import requests from "../utils/requests"
+
 function Nav() {
     return (
         <nav>
-            <div></div>
+            <div>
+                {Object.entries(requests).map(([key, { title, url }]) => (
+                    <h2>{title}</h2>
+                ))}
+            </div>
         </nav>
     )
 }
