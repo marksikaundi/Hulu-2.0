326c1d185509b2b88b833c1d4c3a871bdc4170e7
marksikaundi <emmanuelmark117@gmail.com>
2022-01-08 21:57:32 +0200

    thumbnails build

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { ThumbUpIcon } from "@heroicons/react/outline"
 
 function Thumbnail({ result }) {
     const BASE_URL ="https://image.tmdb.org/t/p/original/"
@@ -6,8 +7,14 @@ function Thumbnail({ result }) {
         <div>
             <Image 
             layout="responsive"
-            src={`${BASE_URL}${result.backdrop_path || result.poster_path}`
-    } height={1000} width={1920} />
+            src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`
+            } height={1000} width={1920} />
+
+            <div className="p-2">
+                <p className="truncate max-w-md">{result.overview}</p>
+                <h2>{result.title || result.original_name}</h2>
+                <p>  </p>
+            </div>
         </div>
     )
 }
