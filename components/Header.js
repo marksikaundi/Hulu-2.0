7ca40d5ab63e368990f28ab0ce9dae7b0ef0bb52
marksikaundi <emmanuelmark117@gmail.com>
2022-01-08 09:23:23 +0200

    header components cleaning

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,21 @@
-import { BadgeCheck, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon, } from "heroicons-react"
+import { 
+    BadgeCheckIcon, 
+    CollectionIcon, 
+    HomeIcon, 
+    LightningBoltIcon, 
+    SearchIcon, 
+    UserIcon,
+ } from "@heroicons/react/outline"
 import Image from "next/image"
 import HeaderItem from "./HeaderItem"
+
 
 function Header() {
     return (
         <header className="">
             <div>
-                <HeaderItem title='Home' Icon={HomeIcon} />
+                <HeaderItem title='HOME' Icon={HomeIcon} /> 
             </div>
-
             <Image 
             className="object-contain"
             src="https://links.papareact.com/ua6"
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../components/Footer'
+// import Footer from '../components/Footer'
 import Header from '../components/Header'
-import Nav from '../components/Nav'
+// import Nav from '../components/Nav'
 
 export default function Home() {
   return (
@@ -18,10 +18,10 @@ export default function Home() {
       {/* header */}
 
       {/* Nav */}
-      <Nav />
+      {/* <Nav /> */}
 
       {/* footer */}
-      <Footer />
+      {/* <Footer /> */}
 
 
 
