d2c3e0ed906cee12d4c392b9ec2977b73ee6030b
marksikaundi <emmanuelmark117@gmail.com>
2022-01-08 13:00:23 +0200

    progress with header

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import {
     BadgeCheckIcon, 
     CollectionIcon, 
     HomeIcon, 
+    LightBulbIcon, 
     LightningBoltIcon, 
     SearchIcon, 
     UserIcon,
@@ -15,6 +16,11 @@ function Header() {
         <header className="">
             <div>
                 <HeaderItem title='HOME' Icon={HomeIcon} /> 
+                <HeaderItem title='TRENDING' Icon={LightningBoltIcon} /> 
+                <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} /> 
+                <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} /> 
+                <HeaderItem title='SEARCH' Icon={SearchIcon} /> 
+                <HeaderItem title='ACCOUNT' Icon={UserIcon} /> 
             </div>
             <Image 
             className="object-contain"
